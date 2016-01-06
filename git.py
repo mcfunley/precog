b0ccf35dda347a2b3e261c016423ac6207edb19a
Michal Migurski <mike@teczno.com>
2016-01-05 22:40:45 -0800

    Cleanup and function docstrings

--- git.py
+++ git.py
@@ -1,6 +1,6 @@
-from os.path import join, exists, dirname
-from os import getcwd, mkdir, environ
-from logging import getLogger
+from os.path import relpath
+from urlparse import urlparse
+from os import environ
 
 from requests_oauthlib import OAuth2Session
 import requests
@@ -8,11 +8,15 @@ import requests
 github_client_id = environ.get('GITHUB_CLIENT_ID') or r'e62e0d541bb6d0125b62'
 github_client_secret = environ.get('GITHUB_CLIENT_SECRET') or r'1f488407e92a59beb897814e9240b5a06a2020e3'
 
-jlogger = getLogger('jekit')
-
 ERR_NO_REPOSITORY = 'Missing repository'
 ERR_PENDING_MESSAGE = 'Test in progress'
 ERR_NO_REF_STATUS = 'Missing statuses for ref'
+
+_GITHUB_USER_URL = 'https://api.github.com/user'
+_GITHUB_REPO_URL = 'https://api.github.com/repos/{owner}/{repo}'
+_GITHUB_REPO_REF_URL = 'https://api.github.com/repos/{owner}/{repo}/git/refs/heads/{ref}'
+_GITHUB_STATUS_URL = 'https://api.github.com/repos/{owner}/{repo}/statuses/{ref}'
+_CIRCLECI_ARTIFACTS_URL = 'https://circleci.com/api/v1/project/{build}/artifacts?circle-token={token}'
 
 class PrivateRepoException (Exception): pass
 
@@ -21,31 +25,31 @@ class MissingRepoException (Exception): pass
 class MissingRefException (Exception): pass
 
 def is_authenticated(access_token):
-    '''
+    ''' Return True if given access token is valid for a Github user.
     '''
     github_auth = access_token, 'x-oauth-basic'
-    user_resp = requests.get('https://api.github.com/user', auth=github_auth)
+    user_resp = requests.get(_GITHUB_USER_URL, auth=github_auth)
     
     return bool(user_resp.status_code == 200)
 
 def repo_exists(owner, repo, access_token):
-    '''
+    ''' Return True if given owner/repo exists in Github.
     '''
     github_auth = access_token, 'x-oauth-basic'
-    repo_url = 'https://api.github.com/repos/{owner}/{repo}'.format(**locals())
+    repo_url = _GITHUB_REPO_URL.format(owner=owner, repo=repo)
     repo_resp = requests.get(repo_url, auth=github_auth)
     
     return bool(repo_resp.status_code == 200)
 
 def find_branch(owner, repo, path, access_token):
-    '''
+    ''' Return existing branch name for a given path - it might contain slashes.
     '''
     github_auth = access_token, 'x-oauth-basic'
     parts = path.split('/')
     
     while parts:
         ref = '/'.join(parts)
-        ref_url = 'https://api.github.com/repos/{owner}/{repo}/git/refs/heads/{ref}'.format(**locals())
+        ref_url = _GITHUB_REPO_REF_URL.format(owner=owner, repo=repo, ref=ref)
         ref_resp = requests.get(ref_url, auth=github_auth)
         
         if ref_resp.status_code == 200:
@@ -53,17 +57,15 @@ def find_branch(owner, repo, path, access_token):
 
         parts.pop()
     
-    pass
+    return None
 
 def get_circle_artifacts(owner, repo, ref, github_token):
+    ''' Return dictionary of CircleCI artifacts for a given Github repo ref.
     '''
-    '''
-    import requests, urlparse, os.path
-    
     circle_token = environ.get('CIRCLECI_TOKEN') or 'a17131792f4c4bcb97f2f66d9c58258a0ee0e621'
     
     github_auth = github_token.get('access_token'), 'x-oauth-basic'
-    status_url = 'https://api.github.com/repos/{owner}/{repo}/statuses/{ref}'.format(**locals())
+    status_url = _GITHUB_STATUS_URL.format(owner=owner, repo=repo, ref=ref)
     status_resp = requests.get(status_url, auth=github_auth)
     
     if status_resp.status_code == 404:
@@ -82,10 +84,10 @@ def get_circle_artifacts(owner, repo, ref, github_token):
         raise RuntimeError('some other test outcome: {state}'.format(**status))
 
     circle_url = status['target_url'] if (status['state'] == 'success') else None
-    circle_build = os.path.relpath(urlparse.urlparse(circle_url).path, '/gh/')
+    circle_build = relpath(urlparse(circle_url).path, '/gh/')
 
-    artifacts_url = 'https://circleci.com/api/v1/project/{}/artifacts?circle-token={}'.format(circle_build, circle_token)
-    artifacts = {os.path.relpath(a['pretty_path'], '$CIRCLE_ARTIFACTS'): '{}?circle-token={}'.format(a['url'], circle_token)
+    artifacts_url = _CIRCLECI_ARTIFACTS_URL.format(build=circle_build, token=circle_token)
+    artifacts = {relpath(a['pretty_path'], '$CIRCLE_ARTIFACTS'): '{}?circle-token={}'.format(a['url'], circle_token)
                  for a in requests.get(artifacts_url, headers=dict(Accept='application/json')).json()}
     
     return artifacts
