fc3414879e86032ccd2a7188a310970cc5ff5607
Dan McKinley <mcfunley@gmail.com>
2016-08-14 19:02:51 -0700

    removing sslify stuff

--- make-it-so.py
+++ make-it-so.py
@@ -36,9 +36,6 @@ webhook_config = parse_webhook_config(*[val for (key, val) in environ.items()
                                         or key == 'WEBHOOK_CONFIG'])
 
 app = Flask(__name__)
-if sys.argv[0] != 'test.py':
-    getLogger('precog').info('SSLifying')
-    SSLify(app)
 app.secret_key = flask_secret_key
 app.jinja_env.filters['nice_relative_time'] = nice_relative_time
 app.config['HOOK_SECRETS_TOKENS'] = webhook_config
@@ -68,74 +65,74 @@ def handle_redirects(untouched_route):
         '''
         # Look for a missing trailing slash at the repository root.
         split_req = request_path.lstrip('/').split('/', 2)
-        
+
         if len(split_req) == 2 and split_req[-1] != '':
             # There are two full components in the path: owner and repo,
             req_owner, req_repo = split_req
-            
+
             if repo_exists(req_owner, req_repo, GET):
                 # Missing a trailing slash for the branch listing.
                 return redirect(absolute_url(request, '{}/'.format(request_path)), 302)
-        
+
         if len(split_req) == 3 and split_req[-1] != '':
             # There are three full components in the path: owner, repo, and ref.
             req_owner, req_repo, req_ref_path = split_req
             req_ref, req_path = split_branch_path(req_owner, req_repo, req_ref_path, GET)
-            
+
             if req_path == '' and not req_ref_path.endswith('/'):
                 # Missing a trailing slash at the root of the repository.
                 return redirect(absolute_url(request, '{}/'.format(request_path)), 302)
-        
+
         return untouched_route(*args, **kwargs)
-    
+
     @wraps(untouched_route)
     def wrapper(*args, **kwargs):
         ''' Redirect under repository root based on referer if necessary.
         '''
         GET = Getter((get_token().get('access_token'), 'x-oauth-basic')).get
-        
+
         # See if the OK hand sign (U+1F44C) was given.
         if request.args.get('go') == u'\U0001f44c':
             return untouched_route(*args, **kwargs)
 
         # See if there's a referer at all.
         referer_url = request.headers.get('Referer')
-        
+
         if not referer_url:
             # No referer, no redirect.
             return maybe_add_slashes(request.path, GET, *args, **kwargs)
-        
+
         # See if the referer path is long enough to suggest a redirect.
         referer_path = urlparse(referer_url).path
         split_path = referer_path.lstrip('/').split('/', 2)
-        
+
         if len(split_path) != 3:
             # Not long enough.
             return maybe_add_slashes(request.path, GET, *args, **kwargs)
-        
+
         # Talk to Github about the path and find a ref name.
         path_owner, path_repo, path_ref = split_path
 
         if not repo_exists(path_owner, path_repo, GET):
             # No repo by this name, no redirect.
             return maybe_add_slashes(request.path, GET, *args, **kwargs)
-        
+
         ref, _ = split_branch_path(path_owner, path_repo, path_ref, GET)
-        
+
         if ref is None:
             # No ref identified, no redirect.
             return maybe_add_slashes(request.path, GET, *args, **kwargs)
-        
+
         # Usually 3, but maybe more?
         slash_count = 2 + len(ref.split('/'))
-        
+
         # See if a redirect is necessary.
         if needs_redirect(request.host, request.path, referer_url, slash_count):
             return make_redirect(slash_count)
-        
+
         # Otherwise, proceed as normal.
         return maybe_add_slashes(request.path, GET, *args, **kwargs)
-    
+
     return wrapper
 
 def handle_authentication(untouched_route):
@@ -154,12 +151,12 @@ def handle_authentication(untouched_route):
 
         access_token = get_token().get('access_token')
         GET = Getter((access_token, 'x-oauth-basic')).get
-    
+
         if access_token is None or not is_authenticated(GET):
             return make_401_response()
-        
+
         return untouched_route(account, repo, *args, **kwargs)
-    
+
     return wrapper
 
 def enforce_signature(route_function):
@@ -173,19 +170,19 @@ def enforce_signature(route_function):
         except:
             return Response(json.dumps({'error': 'Unknown repository'}),
                             401, content_type='application/json')
-            
+
         owner_repo = '{}/{}'.format(owner, repo)
         secret_key = current_app.config['HOOK_SECRETS_TOKENS'].get(owner_repo, {}).get('secret')
-    
+
         #if not secret_key:
         #    # No configured secrets means no signature needed.
         #    getLogger('precog').info('No /hook signature required')
         #    return route_function(*args, **kwargs)
-        
+
         if secret_key is None:
             return Response(json.dumps({'error': 'Missing key'}),
                             401, content_type='application/json')
-    
+
         if 'X-Hub-Signature' not in request.headers:
             # Missing required signature is an error.
             getLogger('precog').warning('No /hook signature provided')
@@ -198,7 +195,7 @@ def enforce_signature(route_function):
 
         actual = request.headers.get('X-Hub-Signature')
         expected = _sign(secret_key)
-        
+
         if actual != expected:
             # Signature mismatch is an error.
             getLogger('precog').warning('Mismatched /hook signatures: {actual} vs. {expected}'.format(**locals()))
@@ -215,12 +212,12 @@ def get_token():
     '''
     token = dict(token_type='bearer', access_token=FAKE_TOKEN)
     token.update(session.get('token', {}))
-    
+
     return token
 
 def make_401_response():
     ''' Create an HTTP 401 Not Authorized response to trigger Github OAuth.
-    
+
         Start by redirecting the user to Github OAuth authorization page:
         http://developer.github.com/v3/oauth/#redirect-users-to-request-github-access
     '''
@@ -228,13 +225,13 @@ def make_401_response():
     states = session.get('states', {})
     states[state_id] = dict(redirect=request.url, created=time())
     session['states'] = states
-    
+
     data = dict(scope='user,repo', client_id=github_client_id, state=state_id)
     href = 'https://github.com/login/oauth/authorize?' + urlencode(data)
-    
+
     auth = make_response(render_template('error-authenticate.html', href=href, codes=err_codes), 401)
     auth.headers['X-Redirect'] = href
-    
+
     return auth
 
 def make_404_response(template, vars):
@@ -249,9 +246,9 @@ def make_500_response(error, traceback):
         message = unicode(error)
     except UnicodeDecodeError:
         message = str(error).decode('latin-1')
-    
+
     vars = dict(error=message, traceback=traceback, codes=err_codes)
-    
+
     return make_response(render_template('error-runtime.html', **vars), 500)
 
 @app.route('/')
@@ -273,7 +270,7 @@ def wellknown_status():
       "resources": { }
     }
     ''' % time()
-    
+
     resp = make_response(status, 200)
     resp.headers['Content-Type'] = 'application/json'
 
@@ -292,38 +289,38 @@ def webhook():
 
     owner, repo, commit_sha, status_url = get_webhook_commit_info(current_app, payload)
     target_path = u'/{owner}/{repo}/{commit_sha}/'.format(**locals())
-    
+
     status = dict(context='mapzen/precog', state='success',
                   target_url=urljoin(request.url, target_path),
                   description=u'Preview your changes')
 
     owner_repo = '{}/{}'.format(owner, repo)
     token = current_app.config['HOOK_SECRETS_TOKENS'].get(owner_repo, {}).get('token')
-    
+
     post_github_status(status_url, status, (token, 'x-oauth-basic'))
-    
+
     return 'Yo.'
 
 @app.route('/oauth/callback')
 @errors_logged
 def get_oauth_callback():
     ''' Handle Github's OAuth callback after a user authorizes.
-    
+
         http://developer.github.com/v3/oauth/#github-redirects-back-to-your-site
     '''
     if 'error' in request.args:
         return render_template('error-oauth.html', reason="you didn't authorize access to your account.")
-    
+
     try:
         code, state_id = request.args['code'], request.args['state']
     except:
         return render_template('error-oauth.html', reason='missing code or state in callback.')
-    
+
     try:
         state = session['states'].pop(state_id)
     except:
         return render_template('error-oauth.html', reason='state "%s" not found?' % state_id)
-    
+
     #
     # Exchange the temporary code for an access token:
     # http://developer.github.com/v3/oauth/#parameters-1
@@ -332,15 +329,15 @@ def get_oauth_callback():
     resp = post('https://github.com/login/oauth/access_token', urlencode(data),
                 headers={'Accept': 'application/json'})
     auth = resp.json()
-    
+
     if 'error' in auth:
         return render_template('error-oauth.html', reason='Github said "%(error)s".' % auth)
-    
+
     elif 'access_token' not in auth:
         return render_template('error-oauth.html', reason="missing `access_token`.")
-    
+
     session['token'] = auth
-    
+
     #
     # Figure out who's here.
     #
@@ -348,7 +345,7 @@ def get_oauth_callback():
     id = OAuth2Session(github_client_id, token=session['token']).get(url).json()
     id = dict(login=id['login'], avatar_url=id['avatar_url'], html_url=id['html_url'])
     session['id'] = id
-    
+
     other = redirect(absolute_url(request, state['redirect']), 302)
     other.headers['Cache-Control'] = 'no-store private'
     other.headers['Vary'] = 'Referer'
@@ -365,7 +362,7 @@ def logout():
 
     if 'token' in session:
         session.pop('token')
-    
+
     return redirect(absolute_url(request, '/'), 302)
 
 @app.route('/<account>/<repo>')
@@ -390,10 +387,10 @@ def repo_only_slash(account, repo):
     GET = Getter((access_token, 'x-oauth-basic'), throws4XX=True).get
     template_args = dict(account=account, repo=repo)
     branches = sorted(get_branch_info(account, repo, GET), key=attrgetter('age'), reverse=False)
-    
+
     if request.args.get('sort') == 'name':
         branches.sort(key=attrgetter('name'))
-    
+
     return render_template('branches.html', branches=branches, **template_args)
 
 @app.route('/<account>/<repo>/<ref>')
@@ -415,15 +412,15 @@ def repo_ref_path(account, repo, ref_path):
     access_token = get_token().get('access_token')
     GET = Getter((access_token, 'x-oauth-basic'), throws4XX=True).get
     template_args = dict(account=account, repo=repo)
-    
+
     if not repo_exists(account, repo, GET):
         return make_404_response('no-such-repo.html', template_args)
-    
+
     ref, path = split_branch_path(account, repo, ref_path, GET)
-    
+
     if ref is None:
         return make_404_response('no-such-ref.html', dict(ref=ref_path, **template_args))
-    
+
     try:
         artifacts = get_circle_artifacts(account, repo, ref, GET)
         artifact_url = artifacts.get(select_path(artifacts, path))
@@ -435,7 +432,7 @@ def repo_ref_path(account, repo, ref_path):
             return make_response(render_template('error-failed.html', **vars), 200)
         else:
             return make_response(render_template('error-runtime.html', **vars), 400)
-    
+
     if artifact_url is None:
         return make_404_response('error-404.html', dict(ref=ref, path=path, **template_args))
 
@@ -452,7 +449,7 @@ def repo_ref_path(account, repo, ref_path):
             mimetype = artifact_resp.headers.get('Content-Type', '')
     except IOError as err:
         return make_response(render_template('error-runtime.html', error=err, codes=err_codes), 500)
-    
+
     return Response(content, headers={'Content-Type': mimetype, 'Cache-Control': 'no-store private'})
 
 @app.route('/<path:path>')
